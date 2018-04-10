264551b2955716a5f2870ea2760cdc829097e78b
thatruth <thankgodegbo@gmail.com>
2018-04-10 14:31:13 +0100

    Task 2

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Internship 4</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background: url(Website-Design-Background.jpg); ">
+
+<div class="intro">
+	HI! I'M THANKGOD EGBO
+</div>
+
+<div class="date">
+  <?php
+echo  date("d");
+?>   
+<div class="month">
+<?php
+$current_time = urlencode(date("F"));
+echo  $current_time;
+?>                
+
+<div class="time">
+<?php
+echo  date("h:ia");
+?>        
+</div>
+
+</div>
+</div>
+
+
+
+
+
+<div class="HNG">
+	HNG INTERNSHIP 4
+	
+	<div class="line">
+	</div>
+	
+	<div class="talk">
+		The Place To Be.....		
+	</div>
+	
+</div>
+
+
+
+
+</body>
+</html>
